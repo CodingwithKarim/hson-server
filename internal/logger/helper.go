ecd6ba9c1d257e410e402ca45ee91d50626de24f
CodingWithKarim <karimhassan92@gmail.com>
2026-08-09 17:18:23 -0400

    HSON-Server 0.4 - Removing utils package

--- internal/logger/helper.go
+++ internal/logger/helper.go
@@ -2,12 +2,24 @@ package logger
 
 import (
 	"fmt"
-	"hson-server/internal/utils"
 	"path/filepath"
 	"strings"
 
 	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/log"
+)
+
+const (
+	colorSlate500    = "#6B7280"
+	colorAlmostWhite = "#F9FAFB"
+	colorCyan300     = "#53EAFD"
+	colorBlue500     = "#3B82F6"
+	colorAmber400    = "#FBBF24"
+	colorRed500      = "#EF4444"
+	colorRed700      = "#C10007"
+	colorGray400     = "#9CA3AF"
+	colorViolet400   = "#A78BFA"
+	colorYellow500   = "#F0B100"
 )
 
 func parseLogLevel(logLevel string) log.Level {
@@ -29,42 +41,42 @@ func customLogStyles() *log.Styles {
 	styles := log.DefaultStyles()
 
 	styles.Timestamp = styles.Timestamp.
-		Foreground(lipgloss.Color(utils.ColorSlate500))
+		Foreground(lipgloss.Color(colorSlate500))
 	styles.Caller = styles.Caller.
-		Foreground(lipgloss.Color(utils.ColorSlate500)).
+		Foreground(lipgloss.Color(colorSlate500)).
 		Italic(true)
 
 	styles.Message = styles.Message.
-		Foreground(lipgloss.Color(utils.ColorAlmostWhite)).
+		Foreground(lipgloss.Color(colorAlmostWhite)).
 		Bold(true)
 
 	styles.Levels[log.DebugLevel] = lipgloss.NewStyle().
 		SetString("DEBUG").
-		Foreground(lipgloss.Color(utils.ColorCyan300)).
+		Foreground(lipgloss.Color(colorCyan300)).
 		Bold(true)
 
 	styles.Levels[log.InfoLevel] = lipgloss.NewStyle().
 		SetString("INFO").
-		Foreground(lipgloss.Color(utils.ColorBlue500)).
+		Foreground(lipgloss.Color(colorBlue500)).
 		Bold(true)
 
 	styles.Levels[log.WarnLevel] = lipgloss.NewStyle().
 		SetString("WARN").
-		Foreground(lipgloss.Color(utils.ColorYellow500)).
+		Foreground(lipgloss.Color(colorYellow500)).
 		Bold(true)
 
 	styles.Levels[log.ErrorLevel] = lipgloss.NewStyle().
 		SetString("ERROR").
-		Foreground(lipgloss.Color(utils.ColorRed500)).
+		Foreground(lipgloss.Color(colorRed500)).
 		Bold(true)
 
 	styles.Levels[log.FatalLevel] = lipgloss.NewStyle().
 		SetString("FATAL").
-		Foreground(lipgloss.Color(utils.ColorRed700)).
+		Foreground(lipgloss.Color(colorRed700)).
 		Bold(true)
 
-	styles.Key = styles.Key.Foreground(lipgloss.Color(utils.ColorGray400)).Faint(true).PaddingLeft(1).PaddingRight(1)
-	styles.Value = styles.Value.Foreground(lipgloss.Color(utils.ColorViolet400)).PaddingLeft(1)
+	styles.Key = styles.Key.Foreground(lipgloss.Color(colorGray400)).Faint(true).PaddingLeft(1).PaddingRight(1)
+	styles.Value = styles.Value.Foreground(lipgloss.Color(colorViolet400)).PaddingLeft(1)
 
 	return styles
 }
